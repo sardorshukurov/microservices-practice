837a0eb8c74867ca4f1fc4a2b270f992a0a8ac80
sardorshukurov <102730798+sardorshukurov@users.noreply.github.com>
2024-05-21 13:35:24 +0500

    Move[RabbitMQ injection] to Play.Common package

--- Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,28 +1,11 @@
-using MassTransit;
-using MassTransit.Definition;
 using Play.Catalog.Service.Entities;
 using Play.Common.MongoDB;
-using Play.Common.Settings;
-using RabbitMQSettings = Play.Catalog.Service.Settings.RabbitMQSettings;
 
 var builder = WebApplication.CreateBuilder(args);
-var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>()!;
 
 builder.Services
     .AddMongo()
     .AddMongoRepository<Item>("items");
-
-builder.Services.AddMassTransit(x =>
-{
-    x.UsingRabbitMq((context, configurator) =>
-    {
-        var rabbitMQSettings = builder.Configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>()!;
-        configurator.Host(rabbitMQSettings.Host);
-        configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
-    });
-});
-
-builder.Services.AddMassTransitHostedService();
 
 builder.Services.AddControllers(o =>
 {
